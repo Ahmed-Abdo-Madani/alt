791ad9daf1c5024e9be9f0fc4a229208fb930184
Ahmed-Addo-Madani <doni.des.ni@gmail.com>
2021-02-27 10:33:22 -0800

    night shift first

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 
-import SplashScreen from "./app/screens/SplashScreen";
-import ItemDetailsScreen from "./app/screens/ItemDetailsScreen";
-import HomeScreen from "./app/screens/HomeScreen";
 import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
