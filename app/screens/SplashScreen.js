664c4e8759bde5549a8de1b9fd97af80a1e55a1e
Ahmed Abdo Madani <doni.des.ni@gmail.com>
2021-05-08 08:25:03 -0700

    splashing

--- app/screens/SplashScreen.js
+++ app/screens/SplashScreen.js
@@ -1,36 +1,61 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import AppButton from "../components/AppButton";
+import { Entypo } from "@expo/vector-icons";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
 
-import Card from "../components/Card";
-import colors from "../config/colors";
+/* "splash": {
+  "image": "./app/assets/tayarlogo.png",
+  "resizeMode": "cover",
+  "backgroundColor": "#028BFF"
+}, */
 
-const SplashScreen = () => {
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        // await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>alt</Text>
-      <Text style={[styles.text, { fontSize: 24 }]}>Logo</Text>
-      <Card />
-      <AppButton
-        title="(☞ﾟヮﾟ)☞"
-        onPress={() => console.log("Pressed 👌🎉🎉")}
-      />
+    <View
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      onLayout={onLayoutRootView}
+    >
+      <Text>SplashScreen Demo! 👋</Text>
+      <Entypo name="rocket" size={30} />
     </View>
   );
-};
-
-export default SplashScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: colors.primary,
-    paddingHorizontal: 13,
-  },
-  text: {
-    fontSize: 48,
-    color: "white",
-  },
-});
+}
